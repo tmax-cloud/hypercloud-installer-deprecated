3ed5ad7862b9c95e80c6da9796700a90d4fa2a43
seonghoon_jeong <seonghoon_jeong@tmax.co.kr>
2020-05-19 16:05:14 +0900

    [test] test commit
    just test commit
    IMS:N/A

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -17,6 +17,7 @@ export default function Home() {
     }
   }
   return (
+
     <div className={styles.container} data-tid="container">
       {/* <h2>Home</h2>
       <Link to={routes.COUNTER}>to Counter</Link> */}
