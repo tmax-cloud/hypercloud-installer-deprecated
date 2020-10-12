564f343acf8819432c86bdf4d6658f86044014fc
Seonghoon Jeong <seonghoon_jeong@tmax.co.kr>
2020-10-12 16:30:50 +0900

    [feat] caltalog controller 설치 기능 추가 중

---
+++ app/components/install/catalogController/InstallContentsCatalogController4.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import styles from '../InstallContents4.css';
+import CONST from '../../../utils/constants/constant';
+import * as env from '../../../utils/common/env';
+// import FinishImage from '../../../../resources/assets/img_finish_mint.svg';
+import FinishImage from '../../../../resources/assets/img_finish_blue.svg';
+import routes from '../../../utils/constants/routes.json';
+
+function InstallContentsCatalogController4(props: any) {
+  console.debug(InstallContentsCatalogController4.name, props);
+  const { history, match, state } = props;
+
+  const nowEnv = env.loadEnvByName(match.params.envName);
+
+  // nowEnv.deleteProductByName(CONST.PRODUCT.CATALOG_CONTROLLER.NAME);
+  // nowEnv.addProduct({
+  //   name: CONST.PRODUCT.CATALOG_CONTROLLER.NAME,
+  //   operator_version: state.operator_version,
+  //   webhook_version: state.webhook_version,
+  //   console_version: state.console_version
+  // });
+  // // json 파일 저장
+  // env.updateEnv(nowEnv.name, nowEnv);
+
+  const getRegistryJsx = () => {
+    if (state.type) {
+      return (
+        <div style={{ marginBottom: '30px' }}>
+          <div>
+            <span className={['medium', 'thick'].join(' ')}>Type</span>
+          </div>
+          <div>
+            <span className={['medium', 'lightDark'].join(' ')}>
+              {state.type}
+            </span>
+          </div>
+        </div>
+      );
+    }
+  };
+  return (
+    <div className={[styles.wrap, 'childLeftRightCenter'].join(' ')}>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ marginBottom: '50px' }}>
+          <img src={FinishImage} alt="Logo" />
+        </div>
+        <div style={{ marginBottom: '30px' }}>
+          <div>
+            <span className={['medium', 'thick'].join(' ')}>
+              Operator Version
+            </span>
+          </div>
+          <div>
+            <span className={['medium', 'lightDark'].join(' ')}>
+              {state.operator_version}
+            </span>
+          </div>
+        </div>
+        <div style={{ marginBottom: '30px' }}>
+          <div>
+            <span className={['medium', 'thick'].join(' ')}>
+              Webhook Version
+            </span>
+          </div>
+          <div>
+            <span className={['medium', 'lightDark'].join(' ')}>
+              {state.webhook_version}
+            </span>
+          </div>
+        </div>
+        <div style={{ marginBottom: '30px' }}>
+          <div>
+            <span className={['medium', 'thick'].join(' ')}>
+              Console Version
+            </span>
+          </div>
+          <div>
+            <span className={['medium', 'lightDark'].join(' ')}>
+              {state.console_version}
+            </span>
+          </div>
+        </div>
+        {getRegistryJsx()}
+        <div>
+          <Button
+            variant="contained"
+            className={['secondary'].join(' ')}
+            size="large"
+            onClick={() => {
+              history.push(`${routes.INSTALL.HOME}/${nowEnv.name}/main`);
+            }}
+          >
+            완료
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InstallContentsCatalogController4;
